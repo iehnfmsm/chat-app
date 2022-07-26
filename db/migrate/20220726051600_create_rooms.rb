a0366b58477ad1f21e9b5b691ba2899f91844e40
iehnfmsm <sotet3na700t95@gmail.com>
2022-07-26 14:45:54 +0900

    room_usersテーブルの作成

--- db/migrate/20220726051600_create_rooms.rb
+++ db/migrate/20220726051600_create_rooms.rb
@@ -1,6 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true
       t.string :name, null: false
       t.timestamps
     end
